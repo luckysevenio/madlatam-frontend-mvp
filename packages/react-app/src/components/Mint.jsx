07429648158dd002f3fbd6d92c052c3b8f5d0b23
Jorge <jorge.barriosn@usm.cl>
2021-11-26 15:54:40 -0300

    fix: require ipfs

--- packages/react-app/src/components/Mint.jsx
+++ packages/react-app/src/components/Mint.jsx
@@ -42,7 +42,7 @@ export default function Mint(props) {
     },
   ];
 
-  const ipfsAPI = require("ipfs-api");
+  const ipfsAPI = require("ipfs-http-client");
   const ipfs = ipfsAPI({ host: "ipfs.infura.io", port: "5001", protocol: "https" });
 
   const layout = {
